1861bb25599eae0074606f48e164a5de550fc813
Zubair46440 <samirzubair46440@gmail.com>
2025-10-06 14:18:29 +0530

    Assignment

--- Task-04/photo-gallary/README.md
+++ Task-04/photo-gallary/README.md
@@ -1,1 +1,4 @@
+Sorry i used AI Tool for submitting the assignment. But now I completed this assignment by myself with a little more creativity 
+Thank-you for Sharing your Feedback
+
 Assignment Link :- "https://venerable-cocada-fa0c87.netlify.app/"
--- Task-04/photo-gallary/src/App.jsx
+++ Task-04/photo-gallary/src/App.jsx
@@ -2,9 +2,11 @@ import useFetch from "./Hook.js/useFetch";
 import "./App.css";
 
 export default function App() {
-  const { data: products, loader, error } = useFetch(
-    "https://api.escuelajs.co/api/v1/products"
-  );
+  const {
+    data: products,
+    loader,
+    error,
+  } = useFetch("https://api.escuelajs.co/api/v1/products");
 
   if (loader) {
     return (
@@ -26,25 +28,27 @@ export default function App() {
   return (
     <div className="app-container">
       <header className="header">
-        <h1>Products</h1>
+        <h1>Products Gallary</h1>
       </header>
 
       <main className="main-content">
         <div className="product-grid">
           {products &&
-            products.slice(0, 40).map((product) => (
+            products.slice(0, 35).map((product) => (
               <div className="product-card" key={product.id}>
                 <img
-                  src={product.images?.[0] || "https://via.placeholder.com/600"}
-                  alt={product.title}
+                  src={
+                    product.images?.[0] ||
+                    "https://images.unsplash.com/photo-1594322436404-5a0526db4d13?q=80&w=1129&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  }
                 />
+
                 <div className="product-info">
                   <h2>{product.title}</h2>
                   <p className="category">{product.category?.name}</p>
                   <p className="price">${product.price}</p>
                   <button> View Product</button>
                 </div>
-                
               </div>
             ))}
         </div>
